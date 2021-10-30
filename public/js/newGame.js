9d3fd1cf52c01dc18593ec352090c7a593cec2ea
eduardoadoglio <eduardoadoglio4@gmail.com>
2021-10-29 22:19:08 -0300

    Get game data from DOM

--- public/js/newGame.js
+++ public/js/newGame.js
@@ -1,19 +1,43 @@
 let socket = io();
 
 function createGame() {
-  gameData = {
-    questions: [
-      {
-        title: "pergunta",
-        answers: [
-          {
-            title: "resposta",
-            correct: true,
-          },
-        ],
-      },
-    ],
-  };
+  gameData = getGameDataFromDOM();
   socket.emit("newGame", gameData);
   window.location.href = "../";
+}
+
+function getGameDataFromDOM() {
+  let questions = getQuestionsFromDOM();
+  return {
+    questions: questions,
+  };
+}
+
+function getQuestionsFromDOM() {
+  let questions = [];
+  $(".question").each(function (_, question) {
+    let questionTitle = $(this).find(".question-title #question").val();
+    console.log(`-- questionTitle = ${questionTitle}`);
+    let answers = getAnswersFromQuestion(question);
+    questions.push({
+      title: questionTitle,
+      answers: answers,
+    });
+  });
+  return questions;
+}
+
+function getAnswersFromQuestion(question) {
+  let answers = [];
+  $(question)
+    .find(".question-answer")
+    .each(function (_, _) {
+      let answerTitle = $(this).find("#alternative").val();
+      let correctAnswer = $(this).find("#right-answer").prop("checked");
+      answers.push({
+        title: answerTitle,
+        correct: correctAnswer,
+      });
+    });
+  return answers;
 }
