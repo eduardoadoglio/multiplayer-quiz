9f85a3bb54e97403add7c0846d1b08ff56ac8312
eduardoadoglio <eduardoadoglio4@gmail.com>
2021-11-01 07:34:08 -0300

    If localStorage is set on index, display modal

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  let modalId = localStorage.getItem("openModal");
+  modalId = "#no-games-found";
+  if (modalId) {
+    $(modalId).css("display", "flex");
+    $(modalId).animate(
+      {
+        top: "10%",
+      },
+      750,
+      function () {}
+    );
+    localStorage.removeItem("openModal");
+  }
+});
+
+$("#close-modal").click(function () {
+  $(this).parent().css("display", "none");
+});
