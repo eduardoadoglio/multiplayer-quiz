05681a76ed72888688f3925cddd2ee00d635f3ca
eduardoadoglio <eduardoadoglio4@gmail.com>
2021-11-03 14:44:51 -0300

    Remove unwanted require

--- models/answer.js
+++ models/answer.js
@@ -2,8 +2,6 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 ObjectId = Schema.ObjectId;
 
-Answer = require("./answer").AnswerModel;
-
 let answerSchema = Schema({
   title: { type: String },
   correct: { type: Boolean, default: false },
